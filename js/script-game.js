92a2eb866f4ae3933c76908560fc1c94b47dc9ba
Jonas Oliveira Dos Santos <jonasoliveiradossantos@outlook.com.br>
2023-08-13 23:06:33 -0300

    codigo final!

--- js/script-game.js
+++ js/script-game.js
@@ -1,9 +1,24 @@
+//delcarando grid de cartas
 const grid = document.querySelector(".grid");
-
+//declarando span de jogadores
 const spanPlay = document.querySelector(".players");
-
+//declarando timer do jogo
 const timer = document.querySelector(".timer");
+//pegando jogador 1 do local storage
+const player1 = localStorage.getItem("player1");
+//pegando jogador 2 do local storage
+const player2 = localStorage.getItem("player2");
 
+//declarando let para alterar jogador
+let currentPlayer = 1;
+//declarando placar do jogador 1
+let player1Score = 0;
+//declarando placar do jogador 2
+let player2Score = 0;
+
+
+//fazendo o jogo
+//declarando todas as cartas do jogo
 const breads = [
     'pao1',
     'pao2',
@@ -17,112 +32,157 @@ const breads = [
     'pao10',
 ];
 
+//criadando uma função para criar elementos HTML dinamicamente e atribuir classes a eles
 const createElement = (tag, className) => {
     const element = document.createElement(tag);
     element.className = className;
     return element;
 }
 
-let firstCard = "";
-let secondCard = "";
-
-const checkEndGame = () => {
-    const disabledCards = document.querySelectorAll(".disabled-card");
-    clearInterval(this.loop);
-    if (disabledCards.length === 20) {
-        alert("Você ganhou!");
-        //alert(" ${spanPlayer1.innerHTML}Você ganhou em " + timer.innerHTML + " segundos!");
-    }
-}
-
-const checkCards = () => {
-    const firstCardBread = firstCard.getAttribute("data-bread");
-    const secondCardBread = secondCard.getAttribute("data-bread");
-
-    if (firstCardBread === secondCardBread) {
-        firstCard.firstChild.classList.add("disabled-card");
-        secondCard.firstChild.classList.add("disabled-card");
-
-        firstCard = "";
-        secondCard = "";
-
-        checkEndGame();
-    } else {
-        setTimeout(() => {
-            firstCard.classList.remove("reveal-card");
-            secondCard.classList.remove("reveal-card");
-
-            firstCard = "";
-            secondCard = "";
-        }, 1000);
-    }
-}
-
-const revealCard = ({ target }) => {
-
-    if (target.classList.contains("reveal-card")) {
-        return
-    }
-
-    if (firstCard === "") {
-        target.parentNode.classList.add("reveal-card");
-        firstCard = target.parentNode;
-    } else if (secondCard === "") {
-        target.parentNode.classList.add("reveal-card");
-        secondCard = target.parentNode;
-        
-        checkCards();
-    } 
-
-    target.parentNode.classList.add("reveal-card");
-}
-
+//criando uma carta 
 const createCard = (bread) => {
+    //atribuindo a carta a uma div
     const card = createElement("div", "card");
+    //atribuindo a carta a uma div com a classe face
     const front = createElement("div", "face front");
+    //atribuindo a carta a uma div com a classe back
     const back = createElement("div", "face back");
-    
+    //atribuindo a carta a uma div com a classe back e a imagem do pão
     front.style.backgroundImage = `url(../imagens/${bread}.png)`;
+    //atribuindo a carta a classe front
     card.appendChild(front);
+    //atribuindo a carta a classe back
     card.appendChild(back);
-
+    //criando um evento de click para revelar a carta
     card.addEventListener("click", revealCard);
+    //atribuindo a carta a um atributo data-bread
     card.setAttribute("data-bread", bread);
-    
+    //retornando a carta
     return card;
 
 }
 
+//carregando todas as cartas do jogo
 const loadGame = () => {
-
+    //criando um array com todas as cartas e duplicando elas
     const duplicateBreads = [...breads, ...breads];
-
+    //embaralhando as cartas
     const shuffledBreads = duplicateBreads.sort(() => Math.random() - 0.5);
-
+    //criando as cartas no grid usando o array embaralhado conm forEach e a função createCard
     shuffledBreads.forEach((bread) => {
+        //criando a carta
         const card = createCard(bread);
+        //adicionando a carta ao grid
         grid.appendChild(card);
     });
 }
 
+//Criando um timer
 const startTimer = () => {
-
-this.loop = setInterval(() => {
-
-    const currentTime = +timer.innerHTML;
-    timer.innerHTML = currentTime + 1;
-
-
-} ,1000);
+    this.loop = setInterval(() => {//this.loop é uma variavel global que vai ser usada para parar o timer
+        const currentTime = +timer.innerHTML; //pegando o tempo atual do timer e transformando em numero o sinal de + é para transformar em numero
+        timer.innerHTML = currentTime + 1; //adicionando 1 segundo ao timer
+    }, 1000);//tempo para adicionar 1 segundo ao timer
 }
 
-window.onload = () => {
-    const player1 = localStorage.getItem("player1");
-    const player2 = localStorage.getItem("player2");
-    spanPlay.innerHTML = player1 + " X " + player2;
-    startTimer();
-    loadGame();
+//checando se o jogo acabou
+const checkEndGame = () => {
+    //pegando todas as cartas com a classe disabled-card e colocando em um array
+    const disabledCards = document.querySelectorAll(".disabled-card");
+    //this loop é uma variavel global que vai ser usada para parar o timer
+    clearInterval(this.loop);
+
+    //checando se o array disabledCards tem 20 cartas
+    if (disabledCards.length === 20) {
+        //checando quem ganhou
+        if (player1Score > player2Score) {
+            alert(`Jogador ${player1} ganhou com ${player1Score} pontos!`);
+        } else if (player2Score > player1Score) {
+            alert(`Jogador ${player2} ganhou com ${player2Score} pontos!`);
+        } else {
+            alert("Empate!");
+        }
+    }
+}
+
+//variavel para revelar as cartas
+const revealCard = ({ target }) => {//target é o elemento que foi clicado
+    if (target.classList.contains("reveal-card") || target.parentNode.classList.contains("reveal-card")) {//se o elemento clicado ou o pai dele tiver a classe reveal-card
+        return;//retorna e não faz nada
+    }
+
+    if (firstCard === "") {//se a primeira carta for vazia
+        target.parentNode.classList.add("reveal-card");//adiciona a classe reveal-card ao pai do elemento clicado para revelar a carta
+        firstCard = target.parentNode; //atribui a primeira carta ao elemento clicado
+    } else if (secondCard === "") { //se a segunda carta for vazia
+        target.parentNode.classList.add("reveal-card"); //adiciona a classe reveal-card ao pai do elemento clicado para revelar a carta
+        secondCard = target.parentNode; //atribui a segunda carta ao elemento clicado
+        if (firstCard !== secondCard) {//se a primeira carta for diferente da segunda carta
+            checkCards(); //chama a função checkCards
+        }
+    }
+}
+
+//checando as cartas para ver se são iguais ou não
+//declarando as variaveis firstCard e secondCard
+let firstCard = "";
+let secondCard = "";
+//declarando o checkCards para checar as cartas
+const checkCards = () => {
+    //pegando o atributo data-bread da primeira carta
+    const firstCardBread = firstCard.getAttribute("data-bread");
+    //pegando o atributo data-bread da segunda carta
+    const secondCardBread = secondCard.getAttribute("data-bread");
+    //checando se as cartas são iguais
+    if (firstCardBread === secondCardBread) {
+        //adicionando a classe disabled-card para desabilitar as cartas
+        firstCard.firstChild.classList.add("disabled-card");
+        secondCard.firstChild.classList.add("disabled-card");
+        //limpando as variaveis firstCard e secondCard
+        firstCard = "";
+        secondCard = "";
+        //adicionando os pontos ao jogador
+        if (currentPlayer === 1) {
+            player1Score++;
+        } else {
+            player2Score++;
+        }
+        //alterando o jogador
+        //obs
+        spanPlay.innerHTML = `${player1}: ${player1Score} X ${player2}: ${player2Score}`;
+        //chamando a função checkEndGame
+        checkEndGame();
+    } else {
+        setTimeout(() => {
+            //removendo a classe reveal-card para esconder as cartas
+            firstCard.classList.remove("reveal-card");
+            secondCard.classList.remove("reveal-card");
+            //limpando as variaveis firstCard e secondCard
+            firstCard = "";
+            secondCard = "";
+            //alterando o jogador
+            currentPlayer = currentPlayer === 1 ? 2 : 1;
+        }, 1000);//tempo para esconder as cartas
+    }
 }
 
 
 
+window.onload = () => {//quando a pagina carregar
+    spanPlay.innerHTML = player1 + ":" + player1Score + " X " + player2 + ":" + player2Score ;//mostrando o nome dos jogadores e os pontos
+    startTimer();//chamando a função startTimer
+    loadGame();//chamando a função loadGame
+}
+
+playAgainButton.addEventListener("click", () => {//quando clicar no botão jogar novamente vai zerar os pontos e o timer e carregar o jogo novamente
+    player1Score = 0; //zerando os pontos do jogador 1
+    player2Score = 0; //zerando os pontos do jogador 2
+    currentPlayer = 1; //alterando o jogador para o jogador 1
+    timer.innerHTML = "0"; //zerando o timer
+    grid.innerHTML = ""; //limpando o grid
+    loadGame(); //carregando o jogo
+    spanPlay.innerHTML = `${player1}: ${player1Score} X ${player2}: ${player2Score}`; //mostrando o nome dos jogadores e os pontos
+    clearInterval(this.loop); //parando o timer
+    startTimer(); //chamando a função startTimer
+    
+});
--- js/script-login.js
+++ js/script-login.js
@@ -1,24 +1,25 @@
-const input1 = document.querySelector('.login-input1');
-const input2 = document.querySelector('.login-input2');
-const button = document.querySelector('.login-button');
-const form = document.querySelector('.login-form');
 
-const validateInput = ({ target }) => {
-    if (input1.value.length > 3 && input2.value.length > 3) {
-        button.removeAttribute('disabled');
+const input1 = document.querySelector('.login-input1');//pegando o input do jogador 1
+const input2 = document.querySelector('.login-input2');//pegando o input do jogador 2
+const button = document.querySelector('.login-button');//pegando o botão de login
+const form = document.querySelector('.login-form');//pegando o formulário de login
+
+const validateInput = ({ target }) => {//criando uma função para validar os inputs
+    if (input1.value.length > 3 && input2.value.length > 3) {//se o tamanho do input for maior que 3
+        button.removeAttribute('disabled');//habilita o botão
     } else {
-        button.setAttribute('disabled', 'true');
+        button.setAttribute('disabled', 'true');//desabilita o botão
     }
 }
 
-input1.addEventListener('input', validateInput);
-input2.addEventListener('input', validateInput);
+input1.addEventListener('input', validateInput);//adicionando um evento de input para o input1
+input2.addEventListener('input', validateInput);//adicionando um evento de input para o input2
 
-const handleSubmit = (event) => {
-    event.preventDefault();
-    localStorage.setItem('player1', input1.value);
-    localStorage.setItem('player2', input2.value);
-    window.location.href = 'game.html';
+const handleSubmit = (event) => {//criando uma função para o submit do formulário
+    event.preventDefault();//previne o comportamento padrão do submit do formulário que é recarregar a página
+    localStorage.setItem('player1', input1.value);//salva o valor do input1 no local storage
+    localStorage.setItem('player2', input2.value);//salva o valor do input2 no local storage
+    window.location.href = 'game.html';//redireciona para a página do jogo
 }
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', handleSubmit);//adicionando um evento de submit para o formulário
